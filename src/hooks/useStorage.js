03703bf525a8ee61c58b8eb1210556973149e26a
Alisson Henrique <69720681+alissonhc92@users.noreply.github.com>
2024-11-05 15:14:06 -0300

    criando salmento de senha

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -2,15 +2,40 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useStorage = () => {
     const gatItem = async (key) => {
-
+        try{
+            const passwords = await AsyncStorage.getItem(key);
+            return JSON.parse(passwords) || [];
+        }catch(error){
+            console.log("Erro ao buscar", error)
+            return[];
+        }
     }
 
-    const saveItem = async () => {
+    const saveItem = async (key, value) => {
+        try{
+            let passwords = await getItem(key);
+
+            passwords.push(value)
+
+            await AsyncStorage.steItem(key, JSON.stringfy(passwords))
+
+        }catch(error){
+            console.log("ERRO AO SALVAR", error)
+        }
 
     }
 
     const removeItem = async () => {
+        try{
+            let passwords = await getItem(key);
 
+            let myPasswords = passwords.filter( (password) => {
+                return (password !== item)
+            })
+
+        }catch(error){
+            console.log("ERROR AO DELETAR", error)
+        }
     }
 
     return{
